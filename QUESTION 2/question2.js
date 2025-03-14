9d0587762fafa27b42615af12425380b41097e36
unknown <YY449@ccs.local>
2025-03-14 19:05:22 +0800

    JavaScript Fundamentals

---
+++ QUESTION 2/question2.js
@@ -0,0 +1,12 @@
+function delayedGreeting(name) {
+    setTimeout(() => {
+      console.log(`Hello, ${name}!`);
+    }, 2000); // Delay of 2 seconds (2000 milliseconds)
+  }
+  
+  // Example usage:
+  delayedGreeting("Roddy");
+  
+  
+  
+  
